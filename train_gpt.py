b60ed471184bcffcde771fec8620bc0cdc9876ed
Matthew Li <mattqlf25@gmail.com>
2026-03-20 13:32:30 -0400

    Use small batch size (131K tokens/step)
    Small batch gets ~6x more gradient steps per wall-clock second.
    Babel results: 1.1890 BPB sliding window vs 1.2194 baseline roundtrip.

--- train_gpt.py
+++ train_gpt.py
@@ -64,7 +64,7 @@ class Hyperparameters:
     iterations = int(os.environ.get("ITERATIONS", 20000))
     warmdown_iters = int(os.environ.get("WARMDOWN_ITERS", 1200))
     warmup_steps = int(os.environ.get("WARMUP_STEPS", 20))
-    train_batch_tokens = int(os.environ.get("TRAIN_BATCH_TOKENS", 786_432))
+    train_batch_tokens = int(os.environ.get("TRAIN_BATCH_TOKENS", 131_072))
     train_seq_len = int(os.environ.get("TRAIN_SEQ_LEN", 2048))
     eval_seq_len = int(os.environ.get("EVAL_SEQ_LEN", 2048))
     max_wallclock_seconds = float(os.environ.get("MAX_WALLCLOCK_SECONDS", 600.0))
